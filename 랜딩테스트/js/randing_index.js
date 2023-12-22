6f1dea265ee1db96289be708b51459a8cf67a552
knknkn0928 <knknkn0928@naver.com>
2023-12-23 00:41:32 +0900

    랜딩페이지 테스트용

---
+++ 랜딩테스트/js/randing_index.js
@@ -0,0 +1,33 @@
+const section = document.querySelector('section');
+const open = document.querySelector('.open');
+const menu = document.querySelector('.left');
+
+open.addEventListener('click', e => {
+    section.classList.add('on');
+})
+
+
+window.addEventListener("wheel", function(e){
+	e.preventDefault();
+},{passive : false});
+
+var $html = $("html");
+var page = 1;
+var lastPage = $(".page").length;
+
+var timeScrollAnimation = 750;
+
+$html.animate({scrollTop:0},10);
+$(window).on("wheel", function(e){
+ 
+	if($html.is(":animated")) return;
+	if(e.originalEvent.deltaY > 0){
+		if(page== lastPage) return;
+		page++;
+	}else if(e.originalEvent.deltaY < 0){
+		if(page == 1) return;
+		page--;
+	}
+	var posTop = (page-1) * ($(window).height());
+	$html.animate({scrollTop : posTop}, timeScrollAnimation);
+});
